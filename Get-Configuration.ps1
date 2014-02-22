5dbdddcb205a82f80e632859c90feda292197c25
ForNeVeR <neverthness@gmail.com>
2014-02-22 19:15:28 +0700

    Add the GetValue method to the configuration object.

--- Get-Configuration.ps1
+++ Get-Configuration.ps1
@@ -1,4 +1,4 @@
-﻿function Find-Configuration {
+﻿function Get-Configuration {
 	$configFileName = 'SvnUtils.config'
 
 	$directory = Get-Item .
@@ -13,7 +13,26 @@
 	$path = "$($directory.FullName)\$configFileName"
 
 	Write-Host "Found configuration file at $path"
-	$config = [IO.File]::ReadAllText($path)
 
-	Invoke-Expression $config
+	$config = Invoke-Expression ([IO.File]::ReadAllText($path))
+    $config | Add-Member -MemberType ScriptMethod -Name 'GetValue' -Value {
+		param (
+			$Name,
+			$DefaultValue,
+			$Substitute
+		)
+
+		if ($Substitute -ne $null) {
+			$Substitute
+		} else {
+			$value = $this[$Name]
+			if ($value -eq $null) {
+				$DefaultValue
+			} else {
+				$value
+			}
+		}
+	}
+
+	$config
 }
