96aa0a44ddf1c468b2e2f80970a915195a4c7352
ForNeVeR <neverthness@gmail.com>
2014-02-22 21:54:52 +0700

    Switch-Branch: use the configured SVN client.

--- Switch-Branch.ps1
+++ Switch-Branch.ps1
@@ -12,11 +12,12 @@ function Switch-Branch {
 	$Root = $config.GetValue('Root', $null, $Root)
 	$Branches = $config.GetValue('Branches', $null, $Branches)
 	$Postfix = $config.GetValue('Postfix', $null, $Postfix)
+	$svn = $config.GetValue('svn', 'svn')
 
 	$branchUrl = Resolve-Branch $Root $Branches $Postfix $BranchName
 
 	Write-Output "Switching to branch $branchUrl"
-	svn switch $branchUrl
+	& $svn switch $branchUrl
 	if ($?) {
 		Write-Output "Switched successfully to $branchUrl"
 	}
