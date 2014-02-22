14a0ba01abf1ebfe6c4f7df8d798b83c6bedfc62
ForNeVeR <neverthness@gmail.com>
2014-02-22 18:26:37 +0700

    Add Find-Configuration script.

---
+++ Find-Configuration.ps1
@@ -0,0 +1,19 @@
+﻿function Find-Configuration {
+	$configFileName = 'SvnUtils.config'
+
+	$directory = Get-Item .
+	while ($directory -ne $null -and -not (Test-Path "$($directory.FullName)\$configFileName")) {
+		$directory = $directory.Parent
+	}
+
+	if ($directory -eq $null) {
+		throw "Cannot find $configFileName"
+	}
+
+	$path = "$($directory.FullName)\$configFileName"
+
+	Write-Host "Found configuration file at $path"
+	$config = [IO.File]::ReadAllText($path)
+
+	Invoke-Expression $config
+}
