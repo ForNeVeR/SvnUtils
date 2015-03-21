f68ed3816d5b6ac0263cf172515676700d4ee906
Friedrich von Never <friedrich@fornever.me>
2015-03-21 19:38:24 +0600

    Change tests.

--- Merge-SvnBranch.Tests.ps1
+++ Merge-SvnBranch.Tests.ps1
@@ -1,6 +1,14 @@
+$ErrorActionPreference = 'Stop'
+
+Import-Module ..\SvnUtils.psd1 -DisableNameChecking
+
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
+. "$here\Get-Configuration.ps1"
+. "$here\Write-Message.ps1"
+. "$here\Format-Url.ps1"
+. "$here\Switch-SvnBranch_Private.ps1"
 
 Describe "Merge-SvnPatch" {
     Context "With repository" {
@@ -8,15 +16,39 @@ Describe "Merge-SvnPatch" {
         New-Item -ItemType Directory $tempRepoPath
         svnadmin create $tempRepoPath
         svn checkout "file:///$($tempRepoPath -replace '\\', '/')"
-        Set-Location (Split-Path $tempRepoPath -Leaf)
-        # TODO: Create configuration file for current repository
-        # TODO: Create file with 'text1', 'text2' in branch b0
-        # TODO: Create branch b1 with 'text2' -> 'text3'
-        # TODO: Create branch b2 with 'text1' -> 'text3'
-        # TODO: Merge commit of b2 to b1 with two-url merge
+        Push-Location (Split-Path $tempRepoPath -Leaf)
+        try {
+            Copy-Item ..\..\SvnUtils.config .
 
-        It 'calls TortoiseMerge' {
-            $false Should Be $true # TODO: Check file content to be exactly 'text3', 'text3'
+            svn mkdir --parents Project/branches/postfix/b0
+            svn commit -m 'Create repository structure and branch b0.'
+            svn switch ^/Project/branches/postfix/b0 --ignore-ancestry
+
+            'text1', 'text2' | Out-File file.txt -Encoding utf8
+            svn add file.txt
+            svn commit -m 'Create file.txt.'
+
+            # Create branch b1 with 'text2' -> 'text3'
+            Create-SvnBranch b0 b1
+            'text1', 'text3' | Out-File file.txt -Encoding utf8
+            svn commit -m 'text2 -> text3'
+
+            # Create branch b2 with 'text1' -> 'text3'
+            Create-SvnBranch b0 b2
+            'text3', 'text2' | Out-File file.txt -Encoding utf8
+            svn commit -m 'text1 -> text3'
+
+            # Merge commit of b2 to b1 with two-url merge
+            Switch-SvnBranch b1
+            Merge-SvnBranch b0 -Source2Name b2 b1
+            
+            It 'merges diff between two trees to current branch successfully' {
+                $content = (Get-Content file.txt) -join "`n"
+                $expected = 'text3', 'text3' -join "`n"
+                $content | Should Be $expected
+            }
+        } finally {
+            Pop-Location
         }
     }
 }
